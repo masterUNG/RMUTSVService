cd0907f14e59519eef532bbb9aaf9892448f16dd
MasterUNG <phrombutr@gmail.com>
2017-11-07 10:34:25 +0700

    Main Fragment Success

--- app/src/main/java/app/ewtc/masterung/rmutsvservice/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/rmutsvservice/MainActivity.java
@@ -3,11 +3,23 @@ package app.ewtc.masterung.rmutsvservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.ewtc.masterung.rmutsvservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+
+        }
+
+    }   // Main Method
+
+}   // Main Class
